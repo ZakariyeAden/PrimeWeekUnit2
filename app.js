09de3129e767bd957631a5ffd7087bdb51c65c39
ZakariyeAden <zakariye5417@gmail.com>
2023-05-03 11:23:09 -0500

    variables practical

---
+++ app.js
@@ -0,0 +1,15 @@
+//Test if Script is working
+console.log("Hello World");
+
+// Declare and Intialize the Variable
+
+let test = 9;
+
+console.log(`the test is ${test}`);
+
+test = 12;
+
+console.log(`the test is ${test}`);
+
+test = 'text';
+console.log(`the test is ${test}`);
